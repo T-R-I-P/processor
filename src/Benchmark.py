6663d4705973f306f55642543ff2fe30db52ca0d
FATESAIKOU <qzsecftbhhhh@gmail.com>
2016-11-22 15:02:06 +0800

    Benchmark V1

--- src/Benchmark.py
+++ src/Benchmark.py
@@ -1,14 +1,48 @@
 #!/usr/bin/env python
 #argv = Setting.json
 
+# System Imports
 import sys
 import json
+import time
+import Utils
 
+# TensorFlow Inports
+import tensorflow as tf
+
+# For Debug
+import pprint
+
+
+""" Env Paramaters Initialization """
 setting_file = sys.argv[1]
+benchmark_file = sys.argv[2]
+output_file_name = sys.argv[3]
 
-src = open(setting_file)
-content = json.loads(src.read())
-src.close()
 
-print '====== Benchmark ======'
+""" Env Initialization """
+worker_hosts = Utils.getSetting(setting_file, "clusters")
+benchmark_src = Utils.loadFile(benchmark_file)
 
+
+""" TensorFlow Env Initialization """
+cluster = tf.train.ClusterSpec({"worker": worker_hosts})
+device_list = Utils.getDeivceList(worker_hosts)
+
+
+""" Benchmark """
+device_score_list = []
+
+for device in device_list:
+    with tf.device(device):
+        start = time.time()
+        exec(benchmark_src)
+        end = time.time()
+
+        device_score_list.append({'name': device, 'exec-time': end - start})
+
+""" Wrapping """
+
+
+""" Output """
+Utils.saveFile(output_file_name, json.dump(device_score_list))
---
+++ src/Utils.py
@@ -0,0 +1,40 @@
+"""
+Utils...
+
+@author FATESAIKOU
+"""
+
+# System Imports
+import json
+
+# TensorFlow Imports
+from tensorflow.python.client import device_lib
+import tensorflow as tf
+
+def loadFile(filename):
+    src = open(filename, 'r')
+    content = src.read()
+    src.close()
+
+    return content
+
+def getSetting(filename, aim=None):
+    settings = json.loads( loadFile(filename) )
+
+    return settings[aim] if ( aim != None and settings.has_key(aim) ) else settings
+
+def getDeviceList(worker_hosts):
+    device_list = []
+
+    for i in xrange(len(worker_hosts)):
+        now_worker_name = 'job:worker/task:%d' % (i)
+        with tf.device(now_worker_name):
+            device_ids = [ device.name for device in device_lib.list_local_devices()]
+            device_list.extend([ (now_worker_name + device_id) for device_id in device_ids ])
+    
+    return device_list
+
+def saveFile(filename, content):
+    src = open(filename, 'w')
+    src.write(content)
+    src.close()
