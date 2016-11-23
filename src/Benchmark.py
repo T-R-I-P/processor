e0c181e198986ebcb203c8b09e749e7a69555680
FATESAIKOU <qzsecftbhhhh@gmail.com>
2016-11-23 16:06:55 +0800

    Benchmark V0.0 complete

--- src/Benchmark.py
+++ src/Benchmark.py
@@ -4,47 +4,56 @@
 # System Imports
 import sys
 import json
-import time
 import Utils
 
-# TensorFlow Inports
-import tensorflow as tf
 
 # For Debug
 import pprint
 
 
 """ Env Paramaters Initialization """
-benchmark_file = sys.argv[1] if len(sys.argv) > 1 else '../data/Benchmark.py'
-setting_file = sys.argv[2] if len(sys.argv) > 2 else '../data/Setting.json'
-output_file_name = sys.argv[3] if len(sys.argv) > 3 else '../data/benchmark_result.json'
+cpu_benchmark_file = sys.argv[1] if len(sys.argv) > 1 else '../data/Cpu-Benchmark.py'
+gpu_benchmark_file = sys.argv[2] if len(sys.argv) > 2 else '../data/Gpu-Benchmark.py'
+setting_file = sys.argv[3] if len(sys.argv) > 3 else '../data/Setting.json'
+output_file_name = sys.argv[4] if len(sys.argv) > 4 else '../data/benchmark_result.json'
 
 
 """ Env Initialization """
 worker_hosts = Utils.getSetting(setting_file, "clusters")
-benchmark_src = Utils.loadFile(benchmark_file)
+net_test_info = Utils.getSetting(setting_file, "net-test-info")
+benchmark_srcs = {
+    "cpu": Utils.loadFile(cpu_benchmark_file),
+    "gpu": Utils.loadFile(gpu_benchmark_file)
+}
+worker_hosts = [{"host": "master", "port": 22222}]
 
 
-""" TensorFlow Env Initialization """
-cluster = tf.train.ClusterSpec({"worker": worker_hosts})
-device_list = Utils.getDeviceList(worker_hosts)
+""" Network Connection Initialization """
+socks = Utils.createSockets(worker_hosts)
 
 
-""" Benchmark """
-device_score_list = []
-
-for device in device_list:
-    with tf.device(device):
-        start = time.time()
-        exec(benchmark_src)
-        end = time.time()
-
-        device_score_list.append({'name': device, 'exec-time': end - start})
+""" Get Device List """
+device_list = Utils.getDeviceList(worker_hosts, socks)
+pprint.pprint(device_list)
 
 
-""" Wrapping """
-# some wrapping option here
+""" Performance Benchmark """
+execution_scores = Utils.getPerformance(benchmark_srcs, device_list, worker_hosts, socks)
+pprint.pprint(execution_scores)
+
+
+""" Network Benchmark """
+network_scores = Utils.getNetCon(net_test_info, worker_hosts, socks)
+pprint.pprint(network_scores)
+
+
+""" Wrapping Result """
+benchmark_result = {}
 
 
 """ Output """
-Utils.saveFile(output_file_name, json.dumps(device_score_list))
+Utils.saveFile(output_file_name, json.dumps(benchmark_result))
+pprint.pprint(benchmark_result)
+
+""" Ending """
+Utils.endTest(worker_hosts, socks)
--- src/Utils.py
+++ src/Utils.py
@@ -6,49 +6,120 @@ Utils...
 
 # System Imports
 import json
+import socket
 
-# TensorFlow Imports
-from tensorflow.python.client import device_lib
-import tensorflow as tf
+import pprint
 
 def loadFile(filename):
-    src = open(filename, 'r')
-    content = src.read()
-    src.close()
+	src = open(filename, 'r')
+	content = src.read()
+	src.close()
 
-    return content
+	return content
 
 def getSetting(filename, aim=None):
-    settings = json.loads( loadFile(filename) )
+	settings = json.loads( loadFile(filename) )
 
-    return settings[aim] if ( aim != None and settings.has_key(aim) ) else settings
-
-def getDeviceList(worker_hosts):
-    device_list = []
-
-    for i in xrange(len(worker_hosts)):
-        now_worker_name = 'job:worker/task:%d' % (i)
-        with tf.device(now_worker_name):
-            device_ids = [ device.name for device in device_lib.list_local_devices()]
-            device_list.extend([ (now_worker_name + device_id) for device_id in device_ids ])
-
-    return device_list
+	return settings[aim] if ( aim != None and settings.has_key(aim) ) else settings
 
 def getBenchmarkResult(filename):
-  benchmark = json.loads(loadFile(filename))
+	benchmark = json.loads(loadFile(filename))
 
-  return benchmark
+	return benchmark
 
 def getWeightList(filename):
-  weight_list = json.loads(loadFile(filename))
+	weight_list = json.loads(loadFile(filename))
 
-  """ Convert to int """
-  for idx in weight_list:
-    weight_list[idx] = int(weight_list[idx])
+	""" Convert to int """
+	for idx in weight_list:
+		weight_list[idx] = int(weight_list[idx])
 
-  return weight_list
+	return weight_list
 
 def saveFile(filename, content):
-    src = open(filename, 'w')
-    src.write(content)
-    src.close()
+	src = open(filename, 'w')
+	src.write(content)
+	src.close()
+
+def createSockets(worker_hosts):
+	socks = {}
+	for worker in worker_hosts:
+		socks[worker["host"]] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		socks[worker["host"]].connect((worker["host"], worker["port"]))
+
+	return socks
+
+def requestMsg(send_msg, wait_msg, sock, recive_size, ttl=10):
+	while True:
+		ttl -= 1
+		sock.send(send_msg)
+
+		msg = sock.recv(recive_size)
+		if msg == wait_msg or wait_msg == None or ttl == 0:
+			break
+
+	return msg
+
+def endTest(worker_hosts, socks, shutdown_server=False):
+	for worker in worker_hosts:
+		if shutdown_server == True:
+			socks[worker["host"]].send('end-server')
+		else:
+			socks[worker["host"]].send('end')
+
+def getDeviceList(worker_hosts, socks):
+	device_list = {}
+	for worker in worker_hosts:
+		sock = socks[worker["host"]]
+
+		requestMsg('get-device-list', 'Connection-Success', sock, 1024)
+		result_size = int( requestMsg('Exec', None, sock, 1024) )
+		device_ids = json.loads( requestMsg('Got-Size', None, sock, result_size) )
+
+		device_list[worker["host"]] = device_ids
+
+	return device_list
+
+def getPerformance(banchmark_srcs, device_list, worker_hosts, socks):
+	execution_scores = {}
+	for worker in worker_hosts:
+		sock = socks[worker["host"]]
+		test_devices = device_list[worker["host"]]
+		execution_scores[worker["host"]] = {}
+
+		for test_device in test_devices:
+			test_benchmark_src = banchmark_srcs['cpu' if 'cpu' in test_device else 'gpu']
+			test_benchmark_src_size = len(test_benchmark_src)
+		
+			requestMsg('get-performance', 'Connection-Success', sock, 1024)
+			requestMsg(str(test_benchmark_src_size), 'Got-Size', sock, 1024)
+			result_size = int(requestMsg(test_benchmark_src, None, sock, 1024))
+			execution_time = float(requestMsg('Got-Size', None, sock, result_size))
+				
+			execution_scores[worker["host"]][test_device] = execution_time
+
+	return execution_scores
+		
+
+def getNetCon(net_test_info, worker_hosts, socks):
+	""" Net Test Paramater Initialization """
+	connect_host = net_test_info["host"]
+	multiplier = max(1, 100.0 / net_test_info["datasize"])
+	transfer_size = min(100, net_test_info["datasize"])
+
+	""" Iperf Command """
+	sh_command = "iperf -c %s -n %d | grep sec | awk '{print $4}'" % (connect_host, transfer_size * 1024 * 1024) 
+
+	""" Start Run """
+	network_scores = {}
+	for worker in worker_hosts:
+		sock = socks[worker["host"]]
+
+		requestMsg('get-net-con', 'Connection-Success', sock, 1024)
+		requestMsg(sh_command, 'Got-Cmd', sock, 1024)
+		result_size = int(requestMsg('Exec', None, sock, 1024))
+		transfer_time = float(requestMsg('Got-Size', None, sock, result_size))
+
+		network_scores[worker["host"]] = max(0.000001, transfer_time * multiplier)
+
+	return network_scores
